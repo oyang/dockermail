3188ad594d603df9c80620c28386f96ed3ad4ea9
Oliver Yang <oyang@sugarcrm.com>
2015-09-18 14:35:51 -0700

    Adding users schema

---
+++ sql/enron_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    userid VARCHAR(128) NOT NULL,
+    domain VARCHAR(128) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    home VARCHAR(255) NOT NULL,
+    uid INTEGER NOT NULL,
+    gid INTEGER NOT NULL
+);
